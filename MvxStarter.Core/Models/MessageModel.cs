c3899bbfa6010ff26278b79a20f80e1ae345e903
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-12-17 10:24:46 +0100

    models added

---
+++ MvxStarter.Core/Models/MessageModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvxStarter.Core.Models
+{
+    public class MessageModel
+    {
+                public DateTime TimeWritten { get; set; }
+        public string ActiveMessage { get; set; }
+        public string UserAlias { get; set; }
+
+        public MessageModel()
+        {
+
+        }
+        public MessageModel(string _activeMessage, string _userAlias)
+        {
+            TimeWritten = DateTime.Now;
+            ActiveMessage = _activeMessage;
+            UserAlias = _userAlias;
+
+
+        }
+    }
+}
