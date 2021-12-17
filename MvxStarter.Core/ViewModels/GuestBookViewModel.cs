418370a42c3547c00c55004a01e195cb804b5b65
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-12-17 09:26:22 +0100

    CanAddGuest added

--- MvxStarter.Core/ViewModels/GuestBookViewModel.cs
+++ MvxStarter.Core/ViewModels/GuestBookViewModel.cs
@@ -1,6 +1,9 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using MvxStarter.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +12,40 @@ namespace MvxStarter.Core.ViewModels
 {
     public class GuestBookViewModel : MvxViewModel
     {
+
+        public GuestBookViewModel()
+        {
+            AddGuestCommand = new MvxCommand(AddGuest);
+        }
+
+        public bool CanAddGuest => FirstName?.Length > 0 && LastName?.Length > 0;
+
+        public IMvxCommand AddGuestCommand { get; set; }
+
         private string _firstName;
+
+        public void AddGuest()
+        {
+            
+            PersonModel p = new PersonModel
+            {
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+            FirstName = string.Empty;
+            LastName = string.Empty;
+
+            People.Add(p);
+        }
+
+        private ObservableCollection<PersonModel> _people = new ObservableCollection<PersonModel>();
+
+        public ObservableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people, value); }
+        }
 
         public string FirstName
         {
@@ -17,8 +53,8 @@ namespace MvxStarter.Core.ViewModels
             set
             {
                 SetProperty(ref _firstName, value);
-                RaisePropertyChanged(() => FulNamn);
-               
+                RaisePropertyChanged(() => FullName);
+                RaisePropertyChanged(() => CanAddGuest);
             }
         }
 
@@ -30,18 +66,18 @@ namespace MvxStarter.Core.ViewModels
             set
             {
                 SetProperty(ref _lastName, value);
-                RaisePropertyChanged(() => FulNamn);
-               
+                RaisePropertyChanged(() => FullName);
+                RaisePropertyChanged(() => CanAddGuest);
             }
         }
-        public string FulNamn
+        public string FullName
         {
             get
             {
                 return $"{FirstName} {LastName}";
             }
             set {; }
-            //public string FullName => $"{FirstName} {LastName}";
+            
         }
     }
 }
