be6f186d4f44a656e9b86f8d5bc4abd222a728cb
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-12-17 09:15:38 +0100

    Add project files.

---
+++ MvxStarter.Core/ViewModels/GuestBookViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvxStarter.Core.ViewModels
+{
+    public class GuestBookViewModel : MvxViewModel
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                SetProperty(ref _firstName, value);
+                RaisePropertyChanged(() => FulNamn);
+               
+            }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                SetProperty(ref _lastName, value);
+                RaisePropertyChanged(() => FulNamn);
+               
+            }
+        }
+        public string FulNamn
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+            set {; }
+            //public string FullName => $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ MvxStarter.Wpf/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+
+
+namespace MvxStarter.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : MvxApplication
+    {
+        public App()
+        {
+
+        }
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxWpfSetup<Core.App>>();
+        }
+
+    }
+}
